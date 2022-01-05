70019c6c34e98ff83e050779c8df731630412eb9
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-01-05 18:03:25 -0300

    exercicios

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise8.js
@@ -0,0 +1,6 @@
+// escreva greet abaixo
+const greet = (name, msg = 'Hi') => `${msg} ${name}`;
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
