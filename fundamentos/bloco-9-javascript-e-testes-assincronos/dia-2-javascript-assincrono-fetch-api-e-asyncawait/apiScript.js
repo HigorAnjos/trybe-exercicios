ac8cf1eb429a656fbaaa4f4475138b2c10f5f84f
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-04-08 22:10:32 -0300

    organizando

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
@@ -0,0 +1,20 @@
+// apiScript.js     
+console.log(document.querySelector('#jokeContainer'));
+/*
+
+const API_URL = 'https://icanhazdadjoke.com/';
+
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(data => data.joke);
+
+};
+
+window.onload = () => fetchJoke();*/
